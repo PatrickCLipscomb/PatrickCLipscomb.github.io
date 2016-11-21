837d1138962f1556154965c1561946cc53e391b5
patrick lipscomb <patrick74lipscomb@gmail.com>
2016-11-21 08:01:13 -0800

    remove parallax on mobile

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,6 @@
-
+$(document).ready(function() {
+  var isiOS = /iPhone|iPad|iPod/i.test(navigator.userAgent)
+  if (isiOS){
+    $('.parallax').removeClass('parallax').addClass('mini-pic');
+  }
+})
