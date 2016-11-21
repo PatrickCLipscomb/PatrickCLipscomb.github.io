6068b8c45c577b7ffb826bf60dc888b9f0a1cb55
patrick lipscomb <patrick74lipscomb@gmail.com>
2016-11-21 10:35:36 -0800

    add some fun jquery

--- js/scripts.js
+++ js/scripts.js
@@ -6,4 +6,13 @@ $(document).ready(function() {
     $('pre').removeClass('pre-parallax');
     $('#work-his').removeClass('mini-pic');
   }
+  $('#prof-pic').on('mouseenter', function() {
+    if ($('#prof-pic').hasClass('classy')) {
+      $('#prof-pic').removeClass('classy')
+      $('#prof-pic').addClass('goofy')
+    } else {
+      $('#prof-pic').removeClass('goofy')
+      $('#prof-pic').addClass('classy')
+    }
+  })
 })
